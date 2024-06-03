cf1486d509b3030a45b824c2756df50960f9d76e
alaidine <aa.alimlinde@gmail.com>
2024-06-03 11:36:56 +0400

    use vulkan glfw to render the vm of corewar

---
+++ vm-src/CMakeFiles/corewar-vm.dir/DependInfo.cmake
@@ -0,0 +1,30 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/alaidine/Projects/corewar/vm-src/imgui/backends/imgui_impl_glfw.cpp" "CMakeFiles/corewar-vm.dir/imgui/backends/imgui_impl_glfw.cpp.o" "gcc" "CMakeFiles/corewar-vm.dir/imgui/backends/imgui_impl_glfw.cpp.o.d"
+  "/home/alaidine/Projects/corewar/vm-src/imgui/backends/imgui_impl_vulkan.cpp" "CMakeFiles/corewar-vm.dir/imgui/backends/imgui_impl_vulkan.cpp.o" "gcc" "CMakeFiles/corewar-vm.dir/imgui/backends/imgui_impl_vulkan.cpp.o.d"
+  "/home/alaidine/Projects/corewar/vm-src/imgui/imgui.cpp" "CMakeFiles/corewar-vm.dir/imgui/imgui.cpp.o" "gcc" "CMakeFiles/corewar-vm.dir/imgui/imgui.cpp.o.d"
+  "/home/alaidine/Projects/corewar/vm-src/imgui/imgui_demo.cpp" "CMakeFiles/corewar-vm.dir/imgui/imgui_demo.cpp.o" "gcc" "CMakeFiles/corewar-vm.dir/imgui/imgui_demo.cpp.o.d"
+  "/home/alaidine/Projects/corewar/vm-src/imgui/imgui_draw.cpp" "CMakeFiles/corewar-vm.dir/imgui/imgui_draw.cpp.o" "gcc" "CMakeFiles/corewar-vm.dir/imgui/imgui_draw.cpp.o.d"
+  "/home/alaidine/Projects/corewar/vm-src/imgui/imgui_tables.cpp" "CMakeFiles/corewar-vm.dir/imgui/imgui_tables.cpp.o" "gcc" "CMakeFiles/corewar-vm.dir/imgui/imgui_tables.cpp.o.d"
+  "/home/alaidine/Projects/corewar/vm-src/imgui/imgui_widgets.cpp" "CMakeFiles/corewar-vm.dir/imgui/imgui_widgets.cpp.o" "gcc" "CMakeFiles/corewar-vm.dir/imgui/imgui_widgets.cpp.o.d"
+  "/home/alaidine/Projects/corewar/vm-src/main.cpp" "CMakeFiles/corewar-vm.dir/main.cpp.o" "gcc" "CMakeFiles/corewar-vm.dir/main.cpp.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
